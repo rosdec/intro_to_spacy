972fd33e40078e4c3fddb5fb20cfaaa1bf78b443
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2023-02-12 09:27:01 +0100

    ** UPD ** Updated the parser, added visualization

--- 03.parser.py
+++ 03.parser.py
@@ -1,8 +1,14 @@
 import spacy
+from spacy import displacy
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("Apple is looking at buying U.K. startup for $1 billion.")
+doc = nlp("Apple, a big  tech company, is looking at buying U.K. startup for $1 billion. Investors are worried about the final price.")
 
 for token in doc:
-    print(token.text, token.pos_, token.tag_, token.dep_,
-            token.shape_, token.is_alpha, token.is_stop)
+    print(token.text, token.head)
+
+for token in doc:
+    if (token.is_sent_start):
+        print(token.text, token.is_sent_start)
+
+displacy.serve(doc, style="dep")
