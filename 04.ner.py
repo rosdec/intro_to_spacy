f4786ea42b0289df6015fd9d295f90a299a77554
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2023-02-12 17:32:41 +0100

    ** UPD ** Added visualization to ner

--- 04.ner.py
+++ 04.ner.py
@@ -1,7 +1,10 @@
 import spacy
+from spacy import displacy
 
 nlp = spacy.load("en_core_web_sm")
 doc = nlp("Apple is looking at buying U.K. startup for $1 billion.")
 
 for ent in doc.ents:
     print(ent.text, ent.label_)
+
+spacy.displacy.serve(doc, style="ent")
