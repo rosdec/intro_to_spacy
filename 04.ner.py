ab0aee3d4db5a7ed68ba4432606cd8003d92a9a5
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2023-02-12 16:16:58 +0100

    ** UPD ** Removed useless info from ner

--- 04.ner.py
+++ 04.ner.py
@@ -4,4 +4,4 @@ nlp = spacy.load("en_core_web_sm")
 doc = nlp("Apple is looking at buying U.K. startup for $1 billion.")
 
 for ent in doc.ents:
-    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+    print(ent.text, ent.label_)
